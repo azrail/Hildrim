030266a1f0b13ece36c408144fc26a5a651b1825
Sascha Hanke <s.hanke@aheron.de>
2011-06-27 20:47:51 +0200

    Update Episodes

--- app/models/MisoSeries.java
+++ app/models/MisoSeries.java
@@ -76,11 +76,14 @@ public class MisoSeries extends Model {
 		MisoSeries mso = getOnlineSeries(media_id, user);
 		
 		if (!ms.episode_count.equals(mso.episode_count)) {
-			System.out.println("ok checking for updates");
+			ms.episode_count = mso.episode_count;
+			ms.season_count = mso.season_count;
+			ms.seasons = mso.seasons;
+			ms.save();
+			Logger.info("Updated Series %s", media_id);
 		}
 		
 		ResultSet rs = DB.executeQuery("Select count(*) as anzahl from MisoEpisode where media_id = " + media_id);
-		
 		Long anzahl = 0L;
 		
 		try {
@@ -93,6 +96,9 @@ public class MisoSeries extends Model {
 
 		if (!mso.episode_count.equals(anzahl)) {
 			System.out.println("Saved Episodes ar not Equals to the Episodes online ("+anzahl+"/"+mso.episode_count+")");
+			TreeSet<MisoEpisode> rsme = createOrReplaceEpisodes(media_id, user, ms);
+			ms.episodes.addAll(rsme);		
+			ms.save();
 		}
 		
 	}
@@ -145,7 +151,41 @@ public class MisoSeries extends Model {
 		misoSeries.media_id = media_id;
 		return misoSeries;
 	}
-
+	/**
+	 * @param media_id
+	 * @param user
+	 * @param misoSeries
+	 * @return
+	 */
+	public static TreeSet<MisoEpisode> createOrReplaceEpisodes(Long media_id, User user, MisoSeries misoSeries) {
+		Logger.debug("Creating Episodes... (%s)", media_id);
+		TreeSet<MisoEpisode> rsme = new TreeSet<MisoEpisode>();
+		Long curSeason = 0L;
+		Long curEpisode = 0L;
+		for (Long i = 0L; i < misoSeries.episode_count + 4; i++) {
+			MisoEpisode misoEpisode = MisoEpisode.findEpisode(media_id, curEpisode, curSeason);
+			if (misoEpisode == null) {
+				MisoEpisode me = MisoEpisode.getEpisodeDetails(media_id, user, curEpisode, curSeason);
+				if (me != null) {
+					curSeason = me.season_num;
+					curEpisode = me.episode_num + 1;
+					DateTimeFormatter fmt = DateTimeFormat.forPattern("yyyy-MM-dd");
+					DateTime dt = null;
+					if (me.aired == null || me.aired == "") {
+						dt = fmt.parseDateTime("1970-01-01");
+					} else {
+						dt = fmt.parseDateTime(me.aired);
+					}
+					me.aired_date = dt.toDateTimeISO().toDate();
+					me.save();
+					rsme.add(me);
+				}
+			} else {
+				rsme.add(misoEpisode);
+			}
+		}
+		return rsme;
+	}
 	/**
 	 * @param media_id
 	 * @param user
